cc34700bb50962bd502f8748c5b916569c4804d9
Samet Tunay <79511355+samettunay@users.noreply.github.com>
2022-04-10 16:09:27 +0300

    Add files via upload

---
+++ crypto.go
@@ -0,0 +1,119 @@
+package crypto
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	s "strings"
+
+	"time"
+
+	"github.com/fatih/color"
+	"github.com/gocolly/colly"
+	"github.com/olekukonko/tablewriter"
+
+	"github.com/atomicgo/cursor"
+	"github.com/inancgumus/screen"
+)
+
+type CryptoCurrency struct {
+	url      string
+	Price    []string
+	Name     []string
+	Percent  []string
+	UpOrDown []string
+}
+
+var (
+	crypto    = new(CryptoCurrency)
+	ErrLenght = errors.New("Lenght Error! must coin name > 3")
+)
+
+func clearInfo() {
+	crypto.Name = nil
+	crypto.UpOrDown = nil
+	crypto.Price = nil
+	crypto.Percent = nil
+}
+
+func getCoinsInfo() {
+	c := colly.NewCollector()
+
+	c.OnHTML("li a div div div span", func(e *colly.HTMLElement) {
+		re := regexp.MustCompile("[0-9]")
+		if re.MatchString(e.Text) {
+			temp := s.TrimSpace(e.Text)
+			if s.Contains(e.Text, "%") {
+				crypto.Percent = append(crypto.Percent, temp)
+			} else if s.Contains(e.Text, "+") || s.Contains(e.Text, "-") {
+				if s.Contains(e.Text, "+") {
+					crypto.UpOrDown = append(crypto.UpOrDown, color.RedString(temp))
+				} else if s.Contains(e.Text, "-") {
+					crypto.UpOrDown = append(crypto.UpOrDown, color.GreenString(temp))
+				}
+			} else {
+				crypto.Price = append(crypto.Price, temp)
+			}
+		}
+	})
+
+	c.OnHTML("li a div div div div div", func(e *colly.HTMLElement) {
+		re := regexp.MustCompile("[a-z]")
+		if re.MatchString(e.Text) && s.Contains(e.Text, "USD") {
+			crypto.Name = append(crypto.Name, e.Text)
+		}
+	})
+
+	c.Visit("https://www.google.com/finance/markets/cryptocurrencies")
+
+}
+
+func createTable(row int) {
+	data := [][]string{}
+
+	for i := 0; i < row; i++ {
+		data = append(data, []string{strconv.Itoa(i), crypto.Name[i], crypto.Price[i], crypto.UpOrDown[i], crypto.Percent[i]})
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"#", "Crypto Currency", "Price", "Up & Down", "Percent"})
+
+	for _, v := range data {
+		table.Append(v)
+	}
+	table.Render() // Send output
+	clearInfo()
+}
+
+// Show coins limit 75
+func ShowLiveTable(coin int) {
+	screen.Clear()
+	cursor.Hide()
+	for {
+		screen.MoveTopLeft()
+		time.Sleep(time.Second)
+		getCoinsInfo()
+		createTable(coin)
+	}
+}
+
+// Get coin info CoinInfo("BTC")
+func CoinInfo(name string) {
+
+	if len(name) < 3 {
+		fmt.Print(ErrLenght)
+		return
+	}
+
+	getCoinsInfo()
+
+	for i, nm := range crypto.Name {
+		con := nm[s.Index(nm, "(")+1 : s.Index(nm, "/")]
+		if s.Contains(con, s.ToUpper(name)) {
+			fmt.Println(nm, crypto.Price[i], crypto.UpOrDown[i], crypto.Percent[i])
+		}
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,34 @@
+module github.com/samettunay/crypto
+
+go 1.18
+
+require (
+	github.com/inancgumus/screen v0.0.0-20190314163918-06e984b86ed3
+	github.com/olekukonko/tablewriter v0.0.5
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/antchfx/htmlquery v1.2.4 // indirect
+	github.com/antchfx/xmlquery v1.3.10 // indirect
+	github.com/antchfx/xpath v1.2.0 // indirect
+	github.com/atomicgo/cursor v0.0.1 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/net v0.0.0-20220407224826-aac1ed45d8e3 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+)
